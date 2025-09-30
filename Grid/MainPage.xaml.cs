bd3f87c7a93fbcd922e6d5518e70750ced8d031a
Daniel Nieto Soriano <24470205@campeche.tecnm.mx>
2025-09-30 08:21:22 -0600

    Grid

---
+++ Grid/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Grid
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+      
+    }
+}
